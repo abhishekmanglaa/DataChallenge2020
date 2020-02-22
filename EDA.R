e5a39d7af655fc047dedf1e35b097746293d0025
Abhishek Mangla <abhishekmangla@wireless-10-104-240-129.umd.edu>
2020-02-22 11:40:25 -0500

    Leaflet

--- EDA.R
+++ EDA.R
@@ -1,15 +1,26 @@
 install.packages("readr")
 library(readr)
-<<<<<<< HEAD
-
+library(dplyr)
+library(ggplot2)
+library(rjson)
+library(jsonlite)
+library(leaflet)
+library(RCurl)
 
 TrafficCount <- read_csv("Data/Data_Level3_MWCOG_RegionalTrafficCount.csv")
 
 table(TrafficCount$COUNTY)
-=======
 TrafficCount <- read_csv("Data/Data_Level3_MWCOG_RegionalTrafficCount.csv")
 
 TrafficCount
 
 str(TrafficCount)
 
+ggplot(TrafficCount, aes(x = COUNTY, y = PassCars)) + 
+  geom_bar()
+
+
+m <- leaflet() %>% 
+  addTiles()  %>% 
+  addCircleMarkers(lat = TrafficCount$YCOORD[1] , lng = TrafficCount$XCOORD[1])
+m
