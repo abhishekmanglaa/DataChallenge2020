1dbdf88b1ebd144fa050e1cfec5a7c0b1fef93a6
Marcm97 <marcm97@umd.edu>
2020-02-22 11:08:35 -0500

    Create EDA_marc.R

---
+++ EDA_marc.R
@@ -0,0 +1,15 @@
+install.packages("readr")
+library(readr)
+<<<<<<< HEAD
+
+
+TrafficCount <- read_csv("Data/Data_Level3_MWCOG_RegionalTrafficCount.csv")
+
+table(TrafficCount$COUNTY)
+=======
+TrafficCount <- read_csv("Data/Data_Level3_MWCOG_RegionalTrafficCount.csv")
+
+TrafficCount
+
+str(TrafficCount)
+>>>>>>> e35abb6ec29bb0682fbf65baa208e7834b6838e1
